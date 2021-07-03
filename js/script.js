7261f66f1f6d5b67790cc81e3cfd04d2912e39d4
hawa-maker <hawwawaweru59@gmail.com>
2021-07-03 15:45:52 +0300

    link javascript file to thefolder in the html file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 var button = document.getElementById("submit")
 button = addEventListener("click",submit)
+
+function submit(event){
+    event.preventDefault()
+    alert("Thank you for your message")
+}
